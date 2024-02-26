7f77f9001afdae5c3db27937a92933dd1cb8311c
koosco <rnxogud123@naver.com>
2024-02-27 00:08:50 +0900

    ticketSeller가 audience의 bag을 참조하지 못하도록 수정, ticketSeller는 ticket가격에 대한 처리를 audience에게 위임한다

--- src/main/java/Audience.java
+++ src/main/java/Audience.java
@@ -8,4 +8,14 @@ public class Audience {
     public Bag getBag() {
         return bag;
     }
+
+    public Long buy(Ticket ticket) {
+        bag.setTicket(ticket);
+        if (bag.hasInvitation()) {
+            return 0L;
+        } else {
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
