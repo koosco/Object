168ba757510e0b2568c877771513c39bc76b0c08
koosco <rnxogud123@naver.com>
2024-02-27 23:05:14 +0900

    예제 생성

---
+++ src/main/java/chapter2/Money.java
@@ -0,0 +1,41 @@
+package chapter2;
+
+import java.math.BigDecimal;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public static Money wons(long amount) {
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    public static Money wons(double amount) {
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Money plus(Money amount) {
+        return new Money(this.amount.add(amount.amount));
+    }
+
+    public Money minus(Money amount) {
+        return new Money(this.amount.subtract(amount.amount));
+    }
+
+    public Money times(double percent) {
+        return new Money(this.amount.multiply(
+            BigDecimal.valueOf(percent)
+        ));
+    }
+
+    public boolean isLessThan(Money other) {
+        return amount.compareTo(other.amount) < 0;
+    }
+
+    public boolean ifGraterthan(Money other) {
+        return amount.compareTo(other.amount) >= 0;
+    }
+}
