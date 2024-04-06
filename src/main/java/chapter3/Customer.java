b732ea0afd4e3efc3a4ceebb91d248d6ec24032e
koosco <rnxogud123@naver.com>
2024-04-07 01:12:03 +0900

    chatper3 : 나머지 객체들 추가

---
+++ src/main/java/chapter3/Customer.java
@@ -0,0 +1,11 @@
+package chapter3;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
