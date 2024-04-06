c8664808f9b72fa066f45ca391504dd5c5718ce7
koosco <rnxogud123@naver.com>
2024-04-07 01:03:11 +0900

    chapter3 : 데이터 중심의 movie 객체 추가

---
+++ src/main/java/chapter3/Movie.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+import chapter2.DiscountCondition;
+import chapter2.Money;
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+}
