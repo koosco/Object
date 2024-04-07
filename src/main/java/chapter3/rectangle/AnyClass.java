3d5bb2bb36510ee26538809a1156dc70848c973b
koosco <rnxogud123@naver.com>
2024-04-07 12:41:16 +0900

    rectangle 예제 추가

--- src/main/java/chapter3/rectangle/AnyClass.java
+++ src/main/java/chapter3/rectangle/AnyClass.java
@@ -1,2 +1,12 @@
-package chapter3.rectangle;public class AnyClass {
+package chapter3.rectangle;
+
+
+/**
+ * 이렇게 클라이언트 코드를 이용해 Rectangle 객체의 속성값에 접근하는 것은 캡슐화를 깨뜨리는 것이다
+ */
+public class AnyClass {
+    void anyMethod(Rectangle rectangle, int multiple) {
+        rectangle.setRight(rectangle.getRight() * multiple);
+        rectangle.setBottom(rectangle.getBottom() * multiple);
+    }
 }
--- src/main/java/chapter3/rectangle/Rectangle.java
+++ src/main/java/chapter3/rectangle/Rectangle.java
@@ -1,2 +1,58 @@
-package chapter3.rectangle;public class Rectangle {
+package chapter3.rectangle;
+
+public class Rectangle {
+
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Rectangle(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+
+
+    /**
+     * rectangle 클래스에 속성값을 조절하는 메서드를 두면 외부에 속성값을 노출하지 않을 수 있고
+     * 코드의 중복도 줄일 수 있다
+     */
+    public void enlarge(int multiple) {
+        right *= multiple;
+        bottom *= multiple;
+    }
 }
